7a4e713f13a5f2d56921cdff8fc88141784ab423
JuanDiego <jnendez38@gmail.com>
2025-08-07 19:16:40 -0500

    Primer commit

---
+++ src/main/java/com/example/primer_proyecto/PrimerProyectoApplication.java
@@ -0,0 +1,17 @@
+package com.example.primer_proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+@SpringBootApplication
+public class PrimerProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyectoApplication.class, args);
+	}
+
+	
+}
